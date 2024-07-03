093dc2ae3151428777a47844066b8690de7e181c
Alex <alex.fiendish@gmail.com>
2024-07-03 22:41:11 +0300

    added basic project flow, created controlled form

---
+++ src/components/Stats.js
@@ -0,0 +1,9 @@
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (x%)</em>
+    </footer>
+  );
+}
+
+export default Stats;
